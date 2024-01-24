40359b752d7c385fe14ab4d35eaf096345e980f9
Christopher Bartz <christopher.bartz@canonical.com>
2024-01-24 20:10:58 +0100

    Fix rabbitmq code

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
+import json
+
 import pika
-from flask import request, app
+from flask import request, Flask
 
+HOST='host'
+VIRTUAL_HOST='vhost'
+USER="user"
+PASSWORD="password"
 
-HOST='localhost'
+app = Flask(__name__)
+
 
 @app.route('/post', methods=['POST'])
 def handle_post():
   payload = request.get_json()
-
-  connection = pika.BlockingConnection(parameters=pika.ConnectionParameters(host=HOST))
+  print(payload)
+  connection = pika.BlockingConnection(parameters=pika.ConnectionParameters(host=HOST, virtual_host=VIRTUAL_HOST, credentials=pika.credentials.PlainCredentials(USER, PASSWORD)))
   channel = connection.channel()
-  channel.basic_publish(exchange='test', routing_key='test',
-                        body=bytes(payload, 'utf-8'))
+  channel.queue_declare(queue='test')
+  channel.basic_publish(exchange='', routing_key='test',
+                        body=bytes(str(payload), 'utf-8'))
   connection.close()
+  return 'OK'
 
 
 @app.route('/get', methods=['GET'])
 def handle_get():
-  connection = pika.BlockingConnection()
+  connection = pika.BlockingConnection(parameters=pika.ConnectionParameters(host=HOST, virtual_host=VIRTUAL_HOST, credentials=pika.credentials.PlainCredentials(USER, PASSWORD)))
   channel = connection.channel()
-
-  for method_frame, properties, body in channel.consume('test'):
+  messages = []
+  method_frame, properties, body = next(channel.consume('test'))
     # Display the message parts and acknowledge the message
-    print(method_frame, properties, body)
-    channel.basic_ack(method_frame.delivery_tag)
-
-    # Escape out of the loop after 10 messages
-    if method_frame.delivery_tag == 10:
-      break
+  print(method_frame, properties, body)
+  messages.append(body.decode('utf-8'))
+  channel.basic_ack(method_frame.delivery_tag)
 
   # Cancel the consumer and return any pending messages
   requeued_messages = channel.cancel()
   print('Requeued %i messages' % requeued_messages)
   connection.close()
+  return json.dumps(messages), 200, {'ContentType': 'application/json'}
