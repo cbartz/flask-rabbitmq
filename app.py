93d410d5ee8e4034ac5664d4333cdb1962a347c6
Christopher Bartz <christopher.bartz@canonical.com>
2024-01-25 11:57:44 +0100

    Adapt code to use config

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import json
+import logging
 
 import pika
 from flask import request, Flask, jsonify
-
-HOST='host'
-VIRTUAL_HOST='vhost'
-USER="user"
-PASSWORD="password"
 
 app = Flask(__name__)
 
@@ -20,29 +16,48 @@ def hello():
 @app.route('/post', methods=['POST'])
 def handle_post():
   payload = request.get_json()
-  print(payload)
-  connection = pika.BlockingConnection(parameters=pika.ConnectionParameters(host=HOST, virtual_host=VIRTUAL_HOST, credentials=pika.credentials.PlainCredentials(USER, PASSWORD)))
-  channel = connection.channel()
-  channel.queue_declare(queue='test')
-  channel.basic_publish(exchange='', routing_key='test',
+  logging.warning(payload)
+
+  rabbitmq_uris = app.config["RABBITMQ_URIS"]
+  logging.warning(rabbitmq_uris)
+
+  for name, uri in rabbitmq_uris.items():
+    logging.warning(name)
+    logging.warning(uri)
+    parameters = pika.URLParameters(uri)
+    connection = pika.BlockingConnection(parameters=parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='test')
+    channel.basic_publish(exchange='', routing_key='test',
                         body=bytes(str(payload), 'utf-8'))
-  connection.close()
+    connection.close()
+
   return 'OK'
 
 
 @app.route('/get', methods=['GET'])
 def handle_get():
-  connection = pika.BlockingConnection(parameters=pika.ConnectionParameters(host=HOST, virtual_host=VIRTUAL_HOST, credentials=pika.credentials.PlainCredentials(USER, PASSWORD)))
-  channel = connection.channel()
-  messages = []
-  method_frame, properties, body = next(channel.consume('test'))
-    # Display the message parts and acknowledge the message
-  print(method_frame, properties, body)
-  messages.append(body.decode('utf-8'))
-  channel.basic_ack(method_frame.delivery_tag)
+  rabbitmq_uris = app.config["RABBITMQ_URIS"]
+  logging.warning(rabbitmq_uris)
 
-  # Cancel the consumer and return any pending messages
-  requeued_messages = channel.cancel()
-  print('Requeued %i messages' % requeued_messages)
-  connection.close()
+  messages = []
+
+  for name, uri in rabbitmq_uris.items():
+    logging.warning(name)
+    logging.warning(uri)
+    parameters = pika.URLParameters(uri)
+    connection = pika.BlockingConnection(parameters=parameters)
+
+    channel = connection.channel()
+    method_frame, properties, body = next(channel.consume('test'))
+      # Display the message parts and acknowledge the message
+    logging.warning(method_frame, properties, body)
+    messages.append(body.decode('utf-8'))
+    channel.basic_ack(method_frame.delivery_tag)
+
+    # Cancel the consumer and return any pending messages
+    requeued_messages = channel.cancel()
+    logging.warning('Requeued %i messages' % requeued_messages)
+    connection.close()
+
   return json.dumps(messages), 200, {'ContentType': 'application/json'}
